5f5fd671822541ae3186576439defe04c12194ad
Antony Kalathil George <132806238+ant-x-ny@users.noreply.github.com>
2024-07-07 12:12:57 +0530

    Add files via upload

---
+++ client/src/components/homepage.jsx
@@ -0,0 +1,136 @@
+import './HomeStyle.css';
+function HomePage(){
+    return(
+        <div className='BodyHome'>
+            <div>
+  <div style={{ height: '125px' }} />
+  <div style={{ height: "100%", width: "100%" }}>
+    <h2
+        style={{
+          margin: '20px',
+          fontFamily: "monospace",
+          fontSize: '82px',
+          paddingTop: '5px',
+          paddingLeft: '5px'
+        }}
+      >
+        Products
+      </h2>
+    <div
+      style={{
+        backgroundColor:'white',
+        margin:'0px 40px',
+        height:'1000px',
+        display:'flex',
+        padding:'40px',
+        justifyContent: 'space-evenly',
+        alignItems:'space-between',
+        flexWrap: 'wrap',
+        padding: '20px'
+      }}>
+
+        <div className='prods'>
+        <div style={{
+            backgroundColor:'#6f6f6f',
+            width:'300px',
+            height:'300px',
+            borderRadius: '12px'
+          }}></div>
+          <h1>Title</h1>
+          
+          <h2>Price</h2>
+          <span style={{
+            margin:'5px'
+          }}>Product Description, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi quod iste nesciunt doloribus odio enim impedit magnam sit nam sapiente. Repellendus praesentium itaque iusto sit minima pariatur doloribus, corporis illo!</span>
+          <h3>Rating: X X X X X stars y responeses</h3>
+         </div>
+
+         <div className='prods'>
+        <div style={{
+            backgroundColor:'#6f6f6f',
+            width:'300px',
+            height:'300px',
+            borderRadius: '12px'
+          }}></div>
+          <h1>Title</h1>
+          
+          <h2>Price</h2>
+          <span style={{
+            margin:'5px'
+          }}>Product Description, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi quod iste nesciunt doloribus odio enim impedit magnam sit nam sapiente. Repellendus praesentium itaque iusto sit minima pariatur doloribus, corporis illo!</span>
+          <h3>Rating: X X X X X stars y responeses</h3>
+         </div>
+
+         <div className='prods'>
+        <div style={{
+            backgroundColor:'#6f6f6f',
+            width:'300px',
+            height:'300px',
+            borderRadius: '12px'
+          }}></div>
+          <h1>Title</h1>
+          
+          <h2>Price</h2>
+          <span style={{
+            margin:'5px'
+          }}>Product Description, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi quod iste nesciunt doloribus odio enim impedit magnam sit nam sapiente. Repellendus praesentium itaque iusto sit minima pariatur doloribus, corporis illo!</span>
+          <h3>Rating: X X X X X stars y responeses</h3>
+         </div>
+
+         <div className='prods'>
+        <div style={{
+            backgroundColor:'#6f6f6f',
+            width:'300px',
+            height:'300px',
+            borderRadius: '12px'
+          }}></div>
+          <h1>Title</h1>
+          
+          <h2>Price</h2>
+          <span style={{
+            margin:'5px'
+          }}>Product Description, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi quod iste nesciunt doloribus odio enim impedit magnam sit nam sapiente. Repellendus praesentium itaque iusto sit minima pariatur doloribus, corporis illo!</span>
+          <h3>Rating: X X X X X stars y responeses</h3>
+         </div>
+
+         <div className='prods'>
+        <div style={{
+            backgroundColor:'#6f6f6f',
+            width:'300px',
+            height:'300px',
+            borderRadius: '12px'
+          }}></div>
+          <h1>Title</h1>
+          
+          <h2>Price</h2>
+          <span style={{
+            margin:'5px'
+          }}>Product Description, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi quod iste nesciunt doloribus odio enim impedit magnam sit nam sapiente. Repellendus praesentium itaque iusto sit minima pariatur doloribus, corporis illo!</span>
+          <h3>Rating: X X X X X stars y responeses</h3>
+         </div>
+
+         <div className='prods'>
+        <div style={{
+            backgroundColor:'#6f6f6f',
+            width:'300px',
+            height:'300px',
+            borderRadius: '12px'
+          }}></div>
+          <h1>Title</h1>
+          
+          <h2>Price</h2>
+          <span style={{
+            margin:'5px'
+          }}>Product Description, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi quod iste nesciunt doloribus odio enim impedit magnam sit nam sapiente. Repellendus praesentium itaque iusto sit minima pariatur doloribus, corporis illo!</span>
+          <h3>Rating: X X X X X stars y responeses</h3>
+         </div>
+
+    </div>
+  </div>
+</div>
+
+        </div>
+    )
+}
+
+export default HomePage;
